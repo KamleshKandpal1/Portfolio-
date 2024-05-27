f63582c01db594ffa3eb500670019d8fc67636a1
Kamlesh Kandpal <kkandpal007@gmail.com>
2024-05-27 23:22:00 +0530

    second-commit

---
+++ src/script/progress.js
@@ -0,0 +1,10 @@
+// gsap.from("#skills .progress-bar", {
+//   scrollTrigger: {
+//     tigger: "#skills",
+//     markers: true,
+//   },
+//   width: "0px",
+//   ease: Power2.easeInOut,
+//   duration: 5,
+//   stagger: 0.2,
+// });
