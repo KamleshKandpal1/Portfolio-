fe4506308238c6540f8b47792f4e199a440830c7
Kamlesh Kandpal <kkandpal007@gmail.com>
2024-05-28 16:32:02 +0530

    third-commit

--- src/script/progress.js
+++ src/script/progress.js
@@ -8,3 +8,9 @@
 //   duration: 5,
 //   stagger: 0.2,
 // });
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
