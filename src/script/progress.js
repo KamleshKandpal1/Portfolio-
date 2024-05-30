20be98b02e303e6c52f329ea091758c0a3a6fd28
Kamlesh Kandpal <kkandpal007@gmail.com>
2024-05-30 20:25:00 +0530

    commit

--- src/script/progress.js
+++ src/script/progress.js
@@ -1,13 +1,3 @@
-// gsap.from("#skills .progress-bar", {
-//   scrollTrigger: {
-//     tigger: "#skills",
-//     markers: true,
-//   },
-//   width: "0px",
-//   ease: Power2.easeInOut,
-//   duration: 5,
-//   stagger: 0.2,
-// });
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
@@ -38,3 +28,92 @@ moveToTopButton.addEventListener("click", handleMoveToTop);
 
 // AOS
 AOS.init();
+
+//Toastify.js
+
+// Form Action
+(function () {
+  const form = document.getElementById("contact-form");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    try {
+      const response = await fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      if (response.ok) {
+        console.log("sucess");
+        Toastify({
+          text: "Message Send",
+          duration: 3000, // duration in milliseconds
+          close: false, // display close button
+          gravity: "top", // `top` or `bottom`
+          position: "right", // `left`, `center` or `right`
+          style: {
+            color: "#008a2e", // custom background color
+            padding: "3px 15px",
+            borderRadius: "10px",
+            fontWeight: "500",
+            background: "#f1f1f1",
+            fontSize: "14px",
+            boxShadow: "0 0 5px rgba(0,0,0,0.6)",
+          }, // custom background color
+          stopOnFocus: true, // stop timeout on focus
+          onClick: function () {}, // callback function when the toast is clicked
+        }).showToast();
+        form.reset();
+      } else {
+        const data = await response.json();
+        if (data.errors) {
+          formMessage.textContent = data.errors
+            .map((error) => error.message)
+            .join(", ");
+        } else {
+          console.log("fail");
+        }
+        Toastify({
+          text: "Message Failed",
+          duration: 3000, // duration in milliseconds
+          close: false, // display close button
+          gravity: "top", // `top` or `bottom`
+          position: "right", // `left`, `center` or `right`
+          style: {
+            color: "#e60000", // custom background color
+            padding: "3px 15px",
+            borderRadius: "10px",
+            fontWeight: "500",
+            background: "#f1f1f1",
+            fontSize: "14px",
+            boxShadow: "0 0 5px rgba(0,0,0,0.6)",
+          }, // custom background color
+          stopOnFocus: true, // stop timeout on focus
+          onClick: function () {}, // callback function when the toast is clicked
+        }).showToast();
+      }
+    } catch (error) {
+      console.log("fail");
+      Toastify({
+        text: "Message Failed",
+        duration: 3000, // duration in milliseconds
+        close: false, // display close button
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        style: {
+          color: "#e60000", // custom background color
+          padding: "3px 15px",
+          borderRadius: "10px",
+          fontWeight: "500",
+          background: "#f1f1f1",
+          fontSize: "14px",
+          boxShadow: "0 0 5px rgba(0,0,0,0.6)",
+        },
+        stopOnFocus: true, // stop timeout on focus
+        onClick: function () {}, // callback function when the toast is clicked
+      }).showToast();
+    }
+  });
+})();
